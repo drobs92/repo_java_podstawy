9a4b569382d6cfddd0bd268c92a7f6027432ac3a
patry.dud <patry.dud@gmail.com>
2022-04-09 12:15:47 +0200

    add conversion

---
+++ src/konwersje/JawnaKonwersja.java
@@ -0,0 +1,11 @@
+package konwersje;
+
+public class JawnaKonwersja {
+    public static void main(String[] args) {
+        int a = 500;
+        double b = 600.5;
+        double wynik = a + b;
+
+        System.out.println(wynik);
+    }
+}
---
+++ src/konwersje/Rzutowanie.java
@@ -0,0 +1,11 @@
+package konwersje;
+
+public class Rzutowanie {
+    public static void main(String[] args) {
+
+        double d = 234234.34534634d;
+        float k = (float) d;
+        System.out.println(k);
+    }
+
+}
