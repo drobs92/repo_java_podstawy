6058f41206dbf76af2114563c66379059bf2beb8
patry.dud <patry.dud@gmail.com>
2022-04-09 09:52:54 +0200

    add zadanie 2

--- src/Wizitowka.java
+++ src/Wizitowka.java
@@ -1,6 +1,6 @@
 public class Wizitowka {
 
     public static void main(String[] args) {
-        System.out.println("Imie nazwisko");
+        System.out.println("Imie nazwisko ");
     }
 }
---
+++ src/Zadanie2.java
@@ -0,0 +1,14 @@
+public class Zadanie2 {
+    public static void main(String[] args) {
+        int value_int = 20;
+        String value_string = "Java";
+        double value_double = 23.63d;
+        System.out.println(" "+ value_double+" "+" "+value_int+" "+" "+value_string);
+        System.out.println(value_int);
+        System.out.println(value_string);
+    }
+}
+//Zadanie 2:- zdeklarować zmienną o nazie value_int typu int i przypisać do niej wartość 20
+// - następnie zdeklarować zmienną value_string typu String i przypisać do niej wartość "Java"
+//- zdeklarować zmienną value_double typu double i przypisać do niej wartość 23.63;
+//- na sam koniec wyświetlić wszystkie zmienne
