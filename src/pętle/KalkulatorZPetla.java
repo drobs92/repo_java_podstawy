3a2001cd69b6f962fab212d524a8e7c8d1fe2c65
Robert Piechowski <robert.t.piechowski@gmail.com>
2022-04-11 01:02:38 +0200

    kalkulatorzpetla dziala

--- src/pętle/KalkulatorZPetla.java
+++ src/pętle/KalkulatorZPetla.java
@@ -1,5 +1,6 @@
 package pętle;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class KalkulatorZPetla {
@@ -11,12 +12,15 @@ public class KalkulatorZPetla {
             Scanner scanner = new Scanner(System.in);
             int a = scanner.nextInt();
 
-            System.out.println("podaj pierwszą cyfrę");
+            System.out.println("podaj drugą cyfrę");
             int b = scanner.nextInt();
 
 
-            System.out.println("Jaką operacje mam wykonać, +, -, *, / ?");
+            System.out.println("Jaką operacje mam wykonać, +, -, *, /,%% ?");
             String dzialanie = scanner.next();
+            if (Objects.equals(dzialanie, "/") && b == 0) {
+                System.out.println("Nie dziel przez 0!");
+            }
 
             switch (dzialanie) {
                 case "+":
@@ -32,12 +36,16 @@ public class KalkulatorZPetla {
                     System.out.println("iloczyn to " + iloczyn);
                     break;
                 case "/":
-                    double iloraz = (double) a * (double) b;
+                    double iloraz = (double) a / (double) b;
                     System.out.println("iloraz to " + iloraz);
-                    if (dzialanie != "+" || dzialanie != "-" || dzialanie != "*" || dzialanie != "/") {
-                        System.out.println("Nie ten znak");
-                    }
                     break;
+                case "%%":
+                    double procent = (double) a/(double) b *100;
+                    System.out.println("Procent to :" +procent);
+                    break;
+                default:
+                    System.out.println("nie ten znak");
+
             }
             System.out.println("Czy jeszcze raz? ");
             pytanie = scanner.next();
